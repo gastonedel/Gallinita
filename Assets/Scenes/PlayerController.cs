f7d66daf1e7e8cc1bf0fff74ba95375ed56d7400
gastonedel <102158957+gastonedel@users.noreply.github.com>
2022-05-26 09:10:19 -0300

    Initial commit

---
+++ Assets/Scenes/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+    public float RotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, RotationSpeed,0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0,-RotationSpeed,0);
+        }
+    }
+}
